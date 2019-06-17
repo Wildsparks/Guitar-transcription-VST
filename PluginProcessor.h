8088f479f049492d4ae60a7ef47fda5bc6c07e23
Wildsparks <51312474+Wildsparks@users.noreply.github.com>
2019-06-17 16:43:28 +0200

    Update PluginProcessor.h

--- PluginProcessor.h
+++ PluginProcessor.h
@@ -51,6 +51,74 @@ public:                                                                         
     bool producesMidi() const override;
     bool isMidiEffect() const override;
     double getTailLengthSeconds() const override;
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include <Eigen/Dense>
+#include <thread>
+#include <mutex>
+
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\Estimation.h"
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\Onset.h"
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\PreProcessing.h"
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\Features.h"
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\HarmonicSummation.h"
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\PitchCandidate.h"
+
+#define SCOPESIZE 4096
+#define NBFRET 12
+#define NBSTRING 6
+
+struct Model;
+
+struct result
+{
+	std::vector<bool> isPlayed;
+	std::vector<int> Fret;
+	std::vector<int> String;
+};
+
+//==============================================================================
+/**
+*/
+class Jacode_iiiAudioProcessor  : public AudioProcessor                             //héritage de audioprocessor
+{
+public:                                                                             //déclaration des méthodes et accesseur
+    //==============================================================================
+    Jacode_iiiAudioProcessor();                                                     //constructor
+    ~Jacode_iiiAudioProcessor();                                                    //décontructor
+
+    //=================================METHODE======================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
 
     //==============================================================================
     int getNumPrograms() override;
@@ -67,7 +135,7 @@ public:                                                                         
 	//================================Osciloscope===================================//
 	//==============================================================================//
 
-	void pushNextSampleIntoFifo(float sample, int size) noexcept;
+	void pushNextSampleIntoFifo(bool isPlayed, int fretPlayed, int stringPlayed, int size) noexcept;
 	void drawNextFrameOfSpectrum();
 	bool getNextFFTBlockReady();
 	void setNextFFTBlockReady(bool value);
@@ -79,7 +147,18 @@ public:                                                                         
 
 	void setThresholdValue(int value);
 
-	//===================================MOI========================================
+	//==============================================================================//
+	//================================Prediction====================================//
+	//==============================================================================//
+	
+	float PitchEstimator(std::vector<float> const& segment);
+	float Jacode_iiiAudioProcessor::BetaEstimator();
+	void Jacode_iiiAudioProcessor::FretStringPrediction(float observedPitch, std::vector<float> pitchReference, int nbFret, Model classifier);
+	float Jacode_iiiAudioProcessor::PluckPositionPrediction();
+
+	//==============================================================================//
+	//===================================MOI========================================//
+	//==============================================================================//
 
 	double getAfficheValue();
 
@@ -104,27 +183,52 @@ private:                                                                        
 	double P;
 	Eigen::VectorXd JMatrix;
 	double prediction;
+	std::vector<float> pitchReference;
+
+	//Initialize estimator / classifier implementation constants
+	float        segmentDuration   = 40e-3; // segment duration in seconds.
+	float        LOpen             = 64.3;  // assumed length of all open strings.
+	unsigned int highNbOfHarmonics = 25;    // assumed high number of harmonics(M >> 1).Used for inharmonic pitch estimationand for estimation of plucking position on the fretted string.
+	unsigned int nbOfHarmonicsInit = 5;     // number of harmonics for initial harmonic estimate(B = 0).
+	unsigned int f0LimitsInf       = 75;
+	unsigned int f0LimitsSup       = 700;   // boundaries for f0 search grid in Hz.
+	float        lengthFFT         = pow(2,19);   // Length of  zero - padded FFT.
+	float        betaRes           = 1e-5;  // resolution of search grid for B.
 
 	//onset
 	int counter;
 	std::vector<float> storageActual;
 	std::vector<float> storagePast;
-	std::vector<double> timeOfOnset;
 	int thresholdValue;
 	bool onsetdetected;
+	std::vector<float> newSegment;
 
 	//analyser//
 
-	std::vector<float> DataScopeNote;      
-	std::vector<float> DataScopefifoNote;
+	result DataScope;
+	result DataScopeFifo;
+
 	bool nextFFTBlockReady = false;       //This temporary boolean tells us whether the next FFT block is ready to be rendered.
 	bool newBuffer = true;
-	float scopeDataNote[SCOPESIZE];           //The scopeData float array of size 512 will contain the points to display on the screen.
-	float scopeDataPluck[SCOPESIZE];
+
+	bool scopeDataIsPlayed[SCOPESIZE];           //The scopeData float array of size 512 will contain the points to display on the screen.
+	int scopeDataFret[SCOPESIZE];
+	int scopeDataString[SCOPESIZE];
+
 	int counterAnalyser;
 
+	//result
+	int stringPlayed;
+	int fretPlayed;
+	std::vector<bool> timeOfOnset;
 
-	//temp
+	//tempo
 	std::vector<float> DataLastBuffer;
+
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Jacode_iiiAudioProcessor)
 };
+
+
+
+
+
