136b64c80fba1a4878d2a543968cadc7478bed80
Wildsparks <51312474+Wildsparks@users.noreply.github.com>
2019-06-17 16:42:09 +0200

    Update Onset.h

--- Onset.h
+++ Onset.h
@@ -3,16 +3,10 @@
 #define ONSET_INCLUDED
 
 #include <iostream>
-
-#include <complex>
-#include <valarray>
-#include <algorithm> 
-
 #include <vector>
-#include <numeric>
-#include <string>
-#include <functional>
 
-void Onset(const float* channelData, int bufsize, int sampleRate, std::vector<double>& timeOfOnset, int& counter, std::vector<float>& storageActual, std::vector<float>& storagePast, int const& thresholdValue);
+#include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\Functions.h"
+
+void Onset(const float* channelData, int bufsize, int sampleRate, std::vector<bool>& timeOfOnset, int& counter, std::vector<float>& storageActual, std::vector<float>& storagePast, int const& thresholdValue);
 
 #endif // ONSET_INCLUDED
