32b8c518cdd2a132dcac3ee439683f6ed3797827
Wildsparks <51312474+Wildsparks@users.noreply.github.com>
2019-07-01 18:02:44 +0200

    Update HarmonicSummation.cpp

--- HarmonicSummation.cpp
+++ HarmonicSummation.cpp
@@ -1,67 +1,77 @@
 #include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\HarmonicSummation.h"
 #include <algorithm>
-#define f0GridStep 0.1
 
-std::vector<float> HarmonicIndex(std::vector<float> SegmentFFT, unsigned int sampleRate, unsigned int nbOfHarmonicsInit, double f0Area);
+void HarmonicIndex(const std::vector<float>& SegmentFFT, unsigned int sampleRate, unsigned int nbOfHarmonicsInit, double f0Area, std::vector<int>& harmonics);
 
-float HarmonicSummation(std::vector<float> SegmentFFT,  unsigned int f0LimitsInf, unsigned int f0LimitsSup, unsigned int nbOfHarmonicsInit, unsigned int sampleRate)
+double HarmonicSummation(const std::vector<float>& SegmentFFT,  unsigned int f0LimitsInf, unsigned int f0LimitsSup, unsigned int nbOfHarmonicsInit, unsigned int sampleRate)
 {
 	
 	std::vector<float> f0Grid;
-	std::vector<float> harmonics;
+	std::vector<int> harmonics(nbOfHarmonicsInit);
 	std::vector<float> harmonicsSum;
-	float pitch;
+	double pitch(0);
+
+	double sumHarmo(0);
+	double maxSumHarmo(0);
 
 	for (double i = f0LimitsInf; i < f0LimitsSup; i += 0.1)
 	{
-		f0Grid.push_back(i);
-	}
-
-	harmonicsSum.resize(f0Grid.size(),0);
-
-	for (int i = 0; i < f0Grid.size(); i++)
-	{
-		harmonics=HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, f0Grid[i]);
+		HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, i, harmonics);
+		sumHarmo = 0;
 
 		for (int j = 0; j < harmonics.size(); ++j)
 		{
-			harmonicsSum[i] += SegmentFFT[int(harmonics[j])];
+			sumHarmo += SegmentFFT[(harmonics[j])];
 		}
-		
+
+		if (maxSumHarmo < sumHarmo)
+		{
+			pitch = i;
+			maxSumHarmo = sumHarmo;
+		}
 	}
-
-	pitch = f0Grid[std::max_element(harmonicsSum.begin(), harmonicsSum.end()) - harmonicsSum.begin()];
-
-	/*//for each frequence we get the sum of the value of the exact harmonic.
+	
+	//for each frequence we get the sum of the value of the exact harmonic.
 	//make it more precise :
-	f0Grid.clear();
-	harmonics.clear();
-	harmonicsSum.clear();
 
 	for (double i = pitch-4.0; i < pitch+4.0; i += 0.01)
 	{
-		f0Grid.push_back(i);
-	}
-
-	harmonicsSum.resize(f0Grid.size(), 0);
-
-	for (int i = 0; i < f0Grid.size(); i++)
-	{
-		harmonics = HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, f0Grid[i]);
+		HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, i, harmonics);
+		sumHarmo = 0.0;
 
 		for (int j = 0; j < harmonics.size(); ++j)
 		{
-			harmonicsSum[i] += SegmentFFT[int(harmonics[j])];
+			sumHarmo += SegmentFFT[(harmonics[j])];
 		}
 
+		if (maxSumHarmo < sumHarmo)
+		{
+			pitch = i;
+			maxSumHarmo = sumHarmo;
+		}
 	}
 
-	pitch = f0Grid[std::max_element(harmonicsSum.begin(), harmonicsSum.end()) - harmonicsSum.begin()];
+	for (double i = pitch - 0.1; i < pitch + 0.1; i += 0.001)
+	{
+		HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, i, harmonics);
+		sumHarmo = 0.0;
 
+		for (int j = 0; j < harmonics.size(); ++j)
+		{
+			sumHarmo += SegmentFFT[(harmonics[j])];
+		}
+
+		if (maxSumHarmo < sumHarmo)
+		{
+			pitch = i;
+			maxSumHarmo = sumHarmo;
+		}
+	}
+
+	/*
 	//for each frequence we get the sum of the value of the exact harmonic.
 	//make it more precise :
 	f0Grid.clear();
-	harmonics.clear();
 	harmonicsSum.clear();
 
 	for (double i = pitch - 0.01; i < pitch + 0.01; i += 0.001)
@@ -73,37 +83,32 @@ float HarmonicSummation(std::vector<float> SegmentFFT,  unsigned int f0LimitsInf
 
 	for (int i = 0; i < f0Grid.size(); i++)
 	{
-		harmonics = HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, f0Grid[i]);
+		HarmonicIndex(SegmentFFT, sampleRate, nbOfHarmonicsInit, f0Grid[i], harmonics);
 
 		for (int j = 0; j < harmonics.size(); ++j)
 		{
-			harmonicsSum[i] += SegmentFFT[int(harmonics[j])];
+			harmonicsSum[i] += SegmentFFT[(harmonics[j])];
 		}
 
 	}
 
-	pitch = f0Grid[std::max_element(harmonicsSum.begin(), harmonicsSum.end()) - harmonicsSum.begin()];*/
-
-	/*//return maximum near f0 estimate
-	std::vector<float> lowerIndex= HarmonicIndex(SegmentFFT, sampleRate, 1, pitch - 0.75 * pitch);
-	std::vector<float> upperIndex= HarmonicIndex(SegmentFFT, sampleRate, 1, pitch + 0.75 * pitch);
-
-	pitch = *std::max_element(SegmentFFT.begin() + lowerIndex[0], SegmentFFT.begin() + upperIndex[0])*/
-
+	pitch = f0Grid[std::max_element(harmonicsSum.begin(), harmonicsSum.end()) - harmonicsSum.begin()-1.0];
+	/*
+	//return maximum near f0 estimate
+	std::vector<int> lowerIndex= HarmonicIndex(SegmentFFT, sampleRate, 1, pitch - 0.75 * pitch, harmonics);
+	std::vector<int> upperIndex= HarmonicIndex(SegmentFFT, sampleRate, 1, pitch + 0.75 * pitch, harmonics);
+	pitch = *std::max_element(SegmentFFT.begin() + lowerIndex[0], SegmentFFT.begin() + upperIndex[0])
+	*/
 	return(pitch);
-
 }
 
-
-std::vector<float> HarmonicIndex(std::vector<float> SegmentFFT, unsigned int sampleRate, unsigned int nbOfHarmonicsInit, double f0Area)
+void HarmonicIndex(const std::vector<float>&  SegmentFFT, unsigned int sampleRate, unsigned int nbOfHarmonicsInit, double f0Area, std::vector<int>& harmonics)
 {
-	std::vector<float> harmonics;
 	double NFFT = SegmentFFT.size();
 
-	for (int i = 1; i < nbOfHarmonicsInit; ++i)
+	for (int i = 1; i < nbOfHarmonicsInit+1; ++i)
 	{
-		harmonics.push_back(round((i*f0Area)*(2*NFFT/sampleRate)));
+		harmonics[i - 1.0] = (round((i * f0Area) * (2 * NFFT / sampleRate)));//*2 to get 2^19
 	}
 	
-	return(harmonics);
 }
