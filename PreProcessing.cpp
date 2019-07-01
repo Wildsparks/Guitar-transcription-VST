807bc2b78aa0e2fe76266398ce273289e92b0e00
Wildsparks <51312474+Wildsparks@users.noreply.github.com>
2019-07-01 18:01:51 +0200

    Update PreProcessing.cpp

--- PreProcessing.cpp
+++ PreProcessing.cpp
@@ -1,12 +1,15 @@
 #include "D:\ecler\Documents\Cours\Ingenieur_4A\Stage\Jacode_III\Builds\VisualStudio2019\PreProcessing.h"
-#define LENGTHFFT pow(2,15) //19
-
-void Preprocessing(std::vector<float>& segment)
+#include "fftw3.h"
+#define LENGTHFFT 524288//pow(2,19) //19
+#define REAL 0
+#define IMAG 1//pow(2,19) //19
+void Preprocessingold(std::vector<float>& segment)
 {
 
 	//=====================================================//
 	//==================Hilbert transform==================//
 	//=====================================================//
+
 	if (segment.size() % 2 == 1)
 	{
 		segment.pop_back();
@@ -14,27 +17,26 @@ void Preprocessing(std::vector<float>& segment)
 
 	CArray segmentFreq(segment.size());
 	//windows//
-	std::vector<float> hann(segment.size());
-	Hanning(segment.size(), hann);
+	std::vector<double> Gauss(segment.size());
+	Gaussian(segment.size(), Gauss);
 	//end - windows//
 
 	for (int i = 0;i < segment.size();++i)
 	{
-		segmentFreq[i] = segment[i]* double(hann[i]);
+		segmentFreq[i] = segment[i] *(Gauss[i]);
 	}
 
 	fft(segmentFreq);
-
-	std::vector<int> h(segment.size());
 
 	segmentFreq[0] = 1.0 * segmentFreq[0];
 	for (int i = 1; i < segment.size()/2; ++i)
 	{
 		segmentFreq[i] = 2.0 * segmentFreq[i];
 	}
-	for (int i = segment.size() / 2; i < segment.size(); ++i)
+	segmentFreq[segment.size() / 2] = 1.0 * segmentFreq[segment.size() / 2];
+	for (int i = segment.size() / 2+1; i < segment.size(); ++i)
 	{
-		segmentFreq[i] = 1.0 * segmentFreq[i];
+		segmentFreq[i] = 0.0;
 	}
 
 	ifft(segmentFreq);
@@ -78,3 +80,204 @@ void Preprocessing(std::vector<float>& segment)
 	//==============Zero padding & FFT End=================//
 	//=====================================================//
 }
+/*
+fftw_complex* HilbertFFTForward(std::vector<float>& segment);
+double* HilbertFFTBackward(fftw_complex* out);
+void LastFFTForward(std::vector<float>& segment, double* hilbertOut);
+*/
+void Preprocessing(std::vector<float>& segment)
+{
+	//=====================================================//
+	//==================Hilbert transform==================//
+	//=====================================================//
+
+	unsigned int N = segment.size();
+
+	if (N % 2 == 1)
+	{
+		segment.push_back(0);
+		N++;
+	}
+
+	fftw_complex* out;
+	double* in;
+
+	fftw_plan p;
+	
+	in = (double*)fftw_malloc(sizeof(double) * N);
+	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N); 
+	p = fftw_plan_dft_r2c_1d(N, in, out, FFTW_ESTIMATE);
+
+	//windows//
+	std::vector<double> Gauss(N);
+	Gaussian(N, Gauss);
+	//end - windows//
+
+	for (int i = 0; i < N; ++i)
+	{
+		in[i] = segment[i] * Gauss[i];
+	}
+
+	fftw_execute(p);
+
+	fftw_destroy_plan(p);
+	fftw_free(in);
+
+	double* hilbertOut;
+	hilbertOut = (double*)fftw_malloc(sizeof(double) * N);
+	fftw_plan q;
+	q = fftw_plan_dft_c2r_1d(N, out, hilbertOut, FFTW_ESTIMATE);
+
+	out[0][REAL] = 1.0 * out[0][REAL];
+	out[0][IMAG] = 1.0 * out[0][IMAG];
+	for (int i = 1; i < N / 2; ++i)
+	{
+		out[i][REAL] = 2.0 * out[i][REAL];
+		out[i][IMAG] = 2.0 * out[i][IMAG];
+	}
+
+	out[N / 2][REAL] = 1.0 * out[N / 2][REAL];
+	out[N / 2][IMAG] = 1.0 * out[N / 2][IMAG];
+
+	for (int i = N / 2 + 1; i < N; ++i)
+	{
+		out[i][REAL] = 0.0;
+		out[i][IMAG] = 0.0;
+	}
+
+	fftw_execute(q);
+	fftw_free(out);
+	fftw_destroy_plan(q);
+
+	//=====================================================//
+	//===============Hilbert transform end=================//
+	//=====================================================//
+
+	//=====================================================//
+	//================Zero padding & FFT===================//
+	//=====================================================//
+
+	double* in2;
+	in2 = (double*)fftw_malloc(sizeof(double) * LENGTHFFT);
+	fftw_complex* fftOut;
+	fftOut = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * LENGTHFFT);
+	
+	p = fftw_plan_dft_r2c_1d(LENGTHFFT, in2, fftOut, FFTW_ESTIMATE);
+
+	for (int i = 0; i < N; ++i)
+	{
+		in2[i] = hilbertOut[i];
+
+	}
+	fftw_free(hilbertOut);
+	for (int i = N; i < LENGTHFFT; ++i)
+	{
+		in2[i] = 0.0;
+	}
+
+	fftw_execute(p);
+	fftw_free(in2);
+
+	segment.resize(LENGTHFFT / 2);
+
+	for (int i = 0; i < LENGTHFFT / 2; ++i)
+	{
+		segment[i] = (2 * (pow(fftOut[i][IMAG],2) + pow(fftOut[i][REAL],2))); //10.0 * log10() if you want db and *2 can be remove
+	}
+	fftw_destroy_plan(p);
+	fftw_free(fftOut);
+
+	//=====================================================//
+	//==============Zero padding & FFT End=================//
+	//=====================================================//
+	fftw_cleanup();
+}
+/*
+fftw_complex* HilbertFFTForward(std::vector<float>& segment)
+{
+	fftw_complex in[N], out[N];
+	fftw_plan p;
+	p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+	//windows//
+	std::vector<float> Gauss(segment.size());
+	Gaussian(segment.size(), Gauss);
+	//end - windows//
+
+	for (int i = 0; i < segment.size(); ++i)
+	{
+		in[i][0] = segment[i] * double(Gauss[i]);
+		in[i][1] = 0;
+	}
+	for (int i = segment.size(); i < N; ++i) //pad
+	{
+		in[i][0] = 0;
+		in[i][1] = 0;
+	}
+
+	fftw_execute(p);
+	return(out);
+	fftw_destroy_plan(p);
+
+}
+
+double* HilbertFFTBackward(fftw_complex* out)
+{
+	double hilbertOut[N];
+	fftw_plan q;
+	q = fftw_plan_dft_c2r_1d(N, out, hilbertOut, FFTW_ESTIMATE);
+
+	out[0][0] = 1.0 * out[0][0];
+	out[0][1] = 1.0 * out[0][1];
+	for (int i = 1; i < N / 2; ++i)
+	{
+		out[i][0] = 2.0 * out[i][0];
+		out[i][1] = 2.0 * out[i][1];
+	}
+
+	out[N / 2][0] = 1.0 * out[N / 2][0];
+	out[N / 2][1] = 1.0 * out[N / 2][1];
+
+	for (int i = N / 2 + 1; i < N; ++i)
+	{
+		out[i][0] = 0.0;
+		out[i][1] = 0.0;
+	}
+
+	fftw_execute(q);
+	return(hilbertOut);
+	fftw_destroy_plan(q);
+
+}
+
+void LastFFTForward(std::vector<float>& segment, double* hilbertOut)
+{
+	
+	double fftOut[LENGTHFFT];
+	fftw_complex in2[LENGTHFFT];
+	fftw_plan p;
+	p = fftw_plan_dft_c2r_1d(LENGTHFFT, in2, fftOut, FFTW_ESTIMATE);
+
+	for (int i = 0; i < N; ++i)
+	{
+		in2[i][0] = hilbertOut[i];
+		in2[i][1] = 0.0;
+	}
+
+	for (int i = N; i < LENGTHFFT; ++i)
+	{
+		in2[i][0] = 0.0;
+		in2[i][1] = 0.0;
+	}
+
+	fftw_execute(p);
+
+	segment.resize(LENGTHFFT / 2);
+
+	for (int i = 0; i < LENGTHFFT / 2; ++i)
+	{
+		segment[i] = (2 * pow(std::abs(fftOut[i]), 2)); //10.0 * log10() if you want db
+	}
+	fftw_destroy_plan(p);
+
+}
+*/
